a7ea4d946e3c2587c47667c579a3768f63897e6f
kauser-cse-buet <anik2137@gmail.com>
2015-04-08 15:06:10 +0600

    Update main.cs

--- Custom-Regular-Expression/main.cs
+++ Custom-Regular-Expression/main.cs
@@ -12,8 +12,38 @@ class Program
         int indexA = 0;
         for(int i=0; i<b.Length; i++){
             Console.WriteLine(b[i]);
-            if(b[i] != '*'){
-                
+            if(char.IsLetter(b[i])){
+                if((i+1) < b.length){
+                    if(b[i+1] == "*"){
+                        indexA++;
+                        while(indexA < a.length && a[indexA] == b[i]){
+                            indexA++;
+                        }
+                    }
+                    else{
+                        if(a[indexA] == b[i]){
+                            indexA++;
+                        }
+                    }
+                }
+            }
+            else{
+                if(b[i] == '*''){
+                    continue;
+                }
+                if(b[i] == '.'){
+                   if(b[i+1] == "*"){
+                        indexA++;
+                        while(indexA < a.length){
+                            indexA++;
+                        }
+                    }
+                    else{
+                        if(a[indexA] == b[i]){
+                            indexA++;
+                        }
+                    } 
+                }
             }
         }
         return false;
